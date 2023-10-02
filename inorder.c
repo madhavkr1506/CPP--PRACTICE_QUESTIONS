810c7f33e647496e891ce0f1241ffd1a68db4722
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2023-10-02 15:05:55 +0530

    Add files via upload

---
+++ inorder.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+struct node {
+    int data;
+    struct node * left;
+    struct node * right;
+};
+struct node * createnode(int data){
+    struct node * n;
+    n=(struct node *)malloc(sizeof(struct node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+void preorder(struct node * root){
+    if(root != NULL){
+        printf("%d\t",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void postorder(struct node * root){
+    if(root != NULL){
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d\t",root->data);
+    }
+}
+void inorder(struct node * root){
+    if(root != NULL){
+        inorder(root->left);
+        printf("%d\t",root->data);
+        inorder(root->right);
+    }
+}
+int main(){
+    struct node * p=createnode(4);
+    struct node * p1=createnode(1);
+    struct node * p2=createnode(6);
+    struct node * p3=createnode(5);
+    struct node * p4=createnode(2);
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+    preorder(p);
+    printf("\n");
+    postorder(p);
+    printf("\n");
+    inorder(p);
+}
